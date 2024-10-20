f9e326b56439896e40254ab404c955cae05e7435
Aleksa Vuckovic <aleksa.vuckovic36@gmail.com>
2024-10-20 16:56:09 +0200

    Add run configuration for compile-test

--- compile-test/build.gradle.kts
+++ compile-test/build.gradle.kts
@@ -2,9 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    application
+}
+application {
+    mainClass.set("org.example.MainKt")
 }
 val pluginName = "ir-plugin"
 val plugin = project(":${pluginName}")
+val pluginJar = "${plugin.layout.buildDirectory.get()}/libs/${pluginName}-${plugin.version}.jar"
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -14,9 +19,10 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     dependsOn(":${pluginName}:assemble")
+    inputs.file(pluginJar)
     kotlinOptions {
         freeCompilerArgs += listOf(
-            "-Xplugin=${plugin.layout.buildDirectory.get()}/libs/${pluginName}-${plugin.version}.jar"
+            "-Xplugin=${pluginJar}"
         )
     }
 }
